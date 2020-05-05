7de3ee799e465a116c04e21c7364a9be10cfcbd9
Tommy Boone <tommyboone23@Tommys-MacBook-Air.local>
2020-05-05 13:43:25 -0500

    footer added with links

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
+
+const Footer = () => {
+  return (
+    <MDBFooter color="blue" className="font-small pt-4 mt-4">
+      <MDBContainer fluid className="text-center text-md-left">
+        <MDBRow>
+          <MDBCol md="6">
+            <h4 className="title">Tommy Boone</h4>
+            <br/>
+            <h5>
+                Like what you see? Let's work together.
+        
+            </h5>
+          </MDBCol>
+          <MDBCol md="6">
+            <h5 className="title">Links</h5>
+            <ul>
+              <li className="list-unstyled">
+                <a href="#!">Home</a>
+              </li>
+              <li className="list-unstyled">
+                <a href="#!">About </a>
+              </li>
+              <li className="list-unstyled">
+                <a href="#!">Experience</a>
+              </li>
+              <li className="list-unstyled">
+                <a href="#!">Projects</a>
+              </li>
+              <li className="list-unstyled">
+                <a href="#!">Resumé</a>
+              </li>
+            </ul>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+      <div className="footer-copyright text-center py-3">
+        <MDBContainer fluid>
+          &copy; {new Date().getFullYear()} Copyright
+        </MDBContainer>
+      </div>
+    </MDBFooter>
+  );
+}
+
+export default Footer;
