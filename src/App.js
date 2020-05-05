63c162ab2cad679a81d6747cb971c79ce93abdee
Tommy Boone <tommyboone23@Tommys-MacBook-Air.local>
2020-05-05 13:42:15 -0500

    page layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
-import Navbar from "./components/NavBar/navbar"
-
-
+import React from "react";
+import Navbar from "./components/NavBar/navbar";
+import Jumbotron from "./components/Jumbotron/jumbotron";
+import Footer from "./components/Footer/Footer"
+import Projects from "./components/Projects/Projects"
+import About from "./components/About/About"
+import Contact from "./components/Contact/Contact"
 function App() {
   return (
+    <div>
       <Navbar />
-  )
+      <Jumbotron/>
+      <About />
+      <Projects />
+      <Contact />
+      <Footer />
+
+    </div>
+  );
 }
 
 export default App;
