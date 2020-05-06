3ce172b8c84fd6cf8ca42527f3811b1baa21f0f5
Tommy Boone <tommyboone23@Tommys-MacBook-Air.local>
2020-05-06 11:42:04 -0500

    added animation

--- src/components/Jumbotron/jumbotron.js
+++ src/components/Jumbotron/jumbotron.js
@@ -1,30 +1,43 @@
 import React from "react";
-import { MDBJumbotron, MDBBtn, MDBRow, MDBCol,  MDBCardImage, MDBCardBody, MDBCardTitle, MDBCardText } from "mdbreact";
+import {
+  MDBJumbotron,
+  MDBBtn,
+  MDBRow,
+  MDBCol,
+  MDBCardImage,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBAnimation,
+} from "mdbreact";
 
 const Jumbotron = () => {
   return (
-   
+    <MDBAnimation type="fadeIn" className="delay-1s slow">
       <MDBRow>
         <MDBCol>
           <MDBJumbotron className="p-0">
             <MDBCardImage
               className="img-fluid"
               src="https://mdbootstrap.com/img/Photos/Slides/img%20(134).jpg"
-            />
+            ></MDBCardImage>
             <MDBCardBody className="text-center">
-              <MDBCardTitle className="h3">Hi! My name is Tommy and I am a Full Stack Web Developer</MDBCardTitle>
-              <MDBCardText>
-       
-              </MDBCardText>
-              <MDBBtn href="#" gradient="blue" rounded>
-                View my projects
-              </MDBBtn>
+              <MDBCardTitle className="h3">
+                {" "}
+                <MDBAnimation type="fadeIn" className="delay-2s slow">
+                  <h1>Hi! My name is Tommy. I am a Full-Stack Web Developer</h1>
+                </MDBAnimation>
+              </MDBCardTitle>
+              <MDBAnimation type="flipInX" className="delay-3s">
+                <MDBBtn href="/projects" gradient="blue" rounded>
+                  View my projects
+                </MDBBtn>
+              </MDBAnimation>
             </MDBCardBody>
           </MDBJumbotron>
         </MDBCol>
       </MDBRow>
-
-  )
-}
+    </MDBAnimation>
+  );
+};
 
 export default Jumbotron;
