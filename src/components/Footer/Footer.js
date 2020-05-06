9a5694ccd70ec18eea2802f249eb04a5730f98cd
Tommy Boone <tommyboone23@Tommys-MacBook-Air.local>
2020-05-06 11:43:45 -0500

    added nav links to other pages

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,35 +3,40 @@ import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
 
 const Footer = () => {
   return (
-    <MDBFooter color="blue" className="font-small pt-4 mt-4">
+    <MDBFooter color="blue-gradient" className="font-small pt-4 mt-4 footer">
       <MDBContainer fluid className="text-center text-md-left">
         <MDBRow>
           <MDBCol md="6">
             <h4 className="title">Tommy Boone</h4>
-            <br/>
             <h5>
                 Like what you see? Let's work together.
+                <br />
+                <br />
+                <a href="mailto:tommyboone23@gmail.com">tommyboone23@gmail.com</a>
+        
         
             </h5>
+
           </MDBCol>
           <MDBCol md="6">
             <h5 className="title">Links</h5>
             <ul>
               <li className="list-unstyled">
-                <a href="#!">Home</a>
+                <a href="/">Home</a>
               </li>
               <li className="list-unstyled">
-                <a href="#!">About </a>
+                <a href="/">About </a>
               </li>
               <li className="list-unstyled">
-                <a href="#!">Experience</a>
+                <a href="/contact">Contact</a>
               </li>
               <li className="list-unstyled">
-                <a href="#!">Projects</a>
+                <a href="/projects">Projects</a>
               </li>
               <li className="list-unstyled">
                 <a href="#!">Resumé</a>
               </li>
+           
             </ul>
           </MDBCol>
         </MDBRow>
